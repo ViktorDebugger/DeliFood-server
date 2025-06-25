98204571ac63a749330ebadcd8aa99f24214cf5b
Viktor <viktor.luka.dev@gmail.com>
2025-06-25 16:47:29 +0300

    Init commit

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import admin from "firebase-admin";
-
 import path from "path";
 import { fileURLToPath } from 'url';
 
@@ -11,15 +10,20 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(cors());
 app.use(express.json());
-const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || "{}");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
+
 // ------------------dishes--------------------------------------
 app.get("/api/dishes", async (_, res) => {
   try {
